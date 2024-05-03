07388c3e907db5d85cccc7272a15c6cfd95e4787
ezraabrari <ezraabrari13@gmail.com>
2024-05-03 15:46:11 +0700

    membuat bool list empty

--- 0059_Doublelinkedlist/0059_Doublelinkedlist.cpp
+++ 0059_Doublelinkedlist/0059_Doublelinkedlist.cpp
@@ -90,4 +90,22 @@ void deleteNode() {
 		cout << "\033[31m The record with roll number " << rollNo << "Not Found\033[0" << endl;
 		return;
 	}
+
+	if (current == START) {
+		START = START->next;
+		if (START != NULL) {
+			START->prev = NULL
+		}
+	}
+	else {
+		previous->next = current->next;
+		if (current->next != NULL) {
+			current->next->prev = previous;
+		}
+	}
+
+	delete current;
+	cout << "\x1b[32mRecord with roll number " << rollNo << "deleted\x1b[0m" << endl;
 }
+
+bool listEmpty
