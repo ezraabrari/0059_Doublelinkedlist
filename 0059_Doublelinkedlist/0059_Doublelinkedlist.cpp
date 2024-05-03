c324f4351fa04eacb5c813f331ae27108c640ea2
ezraabrari <ezraabrari13@gmail.com>
2024-05-03 15:51:42 +0700

    prosedur untuk re traverse

--- 0059_Doublelinkedlist/0059_Doublelinkedlist.cpp
+++ 0059_Doublelinkedlist/0059_Doublelinkedlist.cpp
@@ -113,5 +113,18 @@ bool listEmpty() {
 }
 
 void traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecord in ascending order off roll number : " << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
+void retraverse() {
 
 }
