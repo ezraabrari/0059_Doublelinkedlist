0063bd75a92aa40b9ef940487c64f7da2bf79ded
ezraabrari <ezraabrari13@gmail.com>
2024-05-03 15:56:18 +0700

    prosedur untuk search data

--- 0059_Doublelinkedlist/0059_Doublelinkedlist.cpp
+++ 0059_Doublelinkedlist/0059_Doublelinkedlist.cpp
@@ -126,5 +126,21 @@ void traverse() {
 }
 
 void retraverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecord in descending order off roll number are : " << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev
+		}
+	}
+}
+
+void searchData() {
 
 }
